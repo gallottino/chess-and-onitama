685d1a519aaba6a3e4fa929c94a53a289414fb82
gallottino <d.gallotti@outlook.it>
2024-08-29 13:43:13 +0200

    test: rook moveset

--- test/engine/pieces/rook_test.dart
+++ test/engine/pieces/rook_test.dart
@@ -3,14 +3,14 @@ import 'package:chess_onitama/engine/utilities/position.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Bishop legalMoves without collision', () {
+  test('Rook legalMoves without collision', () {
     /// ♔	♕	♖	♗	♘	♙	♚	♛	♜	♝	♞	♟
     var chessboard = [
       ['■', '□', '■', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
       ['■', '□', '■', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
-      ['■', '□', '■', '♝', '■', '□', '■', '□'],
+      ['■', '□', '■', '♜', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
       ['■', '□', '■', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
@@ -22,53 +22,52 @@ void main() {
     actual.sort();
 
     var expected = [
-      const Position(0, 7),
-      const Position(1, 0),
-      const Position(1, 6),
-      const Position(2, 1),
-      const Position(2, 5),
-      const Position(3, 2),
-      const Position(3, 4),
-      const Position(5, 2),
-      const Position(5, 4),
-      const Position(6, 1),
-      const Position(6, 5),
-      const Position(7, 0),
-      const Position(7, 6),
+      const Position(0, 3),
+      const Position(1, 3),
+      const Position(2, 3),
+      const Position(3, 3),
+      const Position(4, 0),
+      const Position(4, 1),
+      const Position(4, 2),
+      const Position(4, 4),
+      const Position(4, 5),
+      const Position(4, 6),
+      const Position(4, 7),
+      const Position(5, 3),
+      const Position(6, 3),
+      const Position(7, 3),
     ];
     expected.sort();
 
     expect(actual, expected);
   });
 
-  test('Bishop legalMoves with collision', () {
+  test('Rook legalMoves with collision', () {
     /// ♔	♕	♖	♗	♘	♙	♚	♛	♜	♝	♞	♟
     var chessboard = [
       ['■', '□', '■', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
-      ['■', '♗', '■', '□', '■', '□', '■', '□'],
+      ['■', '□', '♗', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
-      ['■', '□', '■', '♝', '■', '□', '■', '□'],
-      ['□', '■', '♜', '■', '□', '■', '□', '■'],
+      ['■', '□', '■', '□', '■', '□', '■', '□'],
+      ['□', '■', '♜', '♝', '□', '■', '□', '■'],
       ['■', '□', '■', '□', '■', '□', '■', '□'],
       ['□', '■', '□', '■', '□', '■', '□', '■'],
     ];
 
     var chess = Chess.fromUnicode(chessboard);
 
-    var actual = chess.board[4][3]!.legalMoves(const Position(4, 3));
+    var actual = chess.board[5][2]!.legalMoves(const Position(5, 2));
     actual.sort();
 
     var expected = [
-      const Position(0, 7),
-      const Position(1, 6),
-      const Position(2, 1),
-      const Position(2, 5),
+      const Position(2, 2),
       const Position(3, 2),
-      const Position(3, 4),
-      const Position(5, 4),
-      const Position(6, 5),
-      const Position(7, 6),
+      const Position(4, 2),
+      const Position(5, 0),
+      const Position(5, 1),
+      const Position(6, 2),
+      const Position(7, 2)
     ];
     expected.sort();
 
