d586a65678bd65069a957dc4e559f767ca518d2c
gallottino <d.gallotti@outlook.it>
2024-08-29 14:47:56 +0200

    feat: Knight implementation

--- lib/engine/pieces/knight.dart
+++ lib/engine/pieces/knight.dart
@@ -1,10 +1,21 @@
+import 'package:chess_onitama/engine/moveset/offset_movement.dart';
 import 'package:chess_onitama/engine/pieces/piece.dart';
 import 'package:chess_onitama/engine/utilities/position.dart';
 
-class Knight extends Piece {
-  Knight({required super.chess, required super.chessColor});
+class Knight extends Piece with OffsetMovement {
+  
+  Knight({required super.chess, required super.chessColor}) {
+    maxMoves = 1;
+    offsets = [
+      const Position(-2, -1),
+      const Position(-2, 1),
+      const Position(-1, -2),
+      const Position(-1, 2),
+      const Position(1, -2),
+      const Position(1, 2),
+      const Position(2, -1),
+      const Position(2, 1),
+    ];
+  }
 
-  @override
-  // TODO: implement legalMoves
-  List<Position> legalMoves(Position from) => throw UnimplementedError();
 }
