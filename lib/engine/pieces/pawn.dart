8628f7217f2de68bac69d361f0bf52e24aad8843
gallottino <d.gallotti@outlook.it>
2024-08-29 17:05:46 +0200

    feat: Pawn implementation

--- lib/engine/pieces/pawn.dart
+++ lib/engine/pieces/pawn.dart
@@ -1,11 +1,41 @@
 import 'package:chess_onitama/engine/pieces/piece.dart';
+import 'package:chess_onitama/engine/utilities/color.dart';
 import 'package:chess_onitama/engine/utilities/position.dart';
 
 class Pawn extends Piece {
-  Pawn({required super.chess, required super.chessColor});
+  bool firstMove;
+  Pawn(
+      {required super.chess, required super.chessColor, this.firstMove = true});
 
   @override
   List<Position> legalMoves(Position from) {
-    throw UnimplementedError();
+    List<Position> moves = [];
+    int direction = chessColor == ChessColor.white ? -1 : 1;
+
+    var newPosition = from + Position(direction * 1, 0);
+    var firstIsEmpty = chess.isEmptyAt(newPosition);
+    if (firstIsEmpty) {
+      moves.add(newPosition);
+    }
+
+    newPosition = from + Position(direction * 2, 0);
+    if (firstMove && firstIsEmpty && chess.isEmptyAt(newPosition)) {
+      moves.add(newPosition);
+    }
+
+    var captureOffsets = [
+      Position(direction * 1, 1),
+      Position(direction * 1, -1),
+    ];
+
+    for (var position in captureOffsets) {
+      var captureOffset = from + position;
+      if (chess.isEmptyAt(captureOffset)) continue;
+      if (chess.getPieceAt(captureOffset).chessColor != chessColor) {
+        moves.add(captureOffset);
+      }
+    }
+
+    return moves;
   }
 }
