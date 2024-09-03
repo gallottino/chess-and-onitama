84030d2974f79117b8c712c50bb1ad32c0856eba
gallottino <d.gallotti@outlook.it>
2024-09-03 21:47:53 +0200

    feat: init onitama board

--- lib/engine/onitama.dart
+++ lib/engine/onitama.dart
@@ -1,6 +1,8 @@
 import 'package:chess_onitama/engine/chess.dart';
 import 'package:chess_onitama/engine/pieces/onitama/move_card.dart';
+import 'package:chess_onitama/engine/pieces/onitama/pawn.dart';
 import 'package:chess_onitama/engine/utilities/color.dart';
+import 'package:chess_onitama/engine/utilities/position.dart';
 
 class Onitama extends Chess {
   List<MoveCard>? moveCards = [];
@@ -25,5 +27,31 @@ class Onitama extends Chess {
     return playerCards[turn]![selectedCardIndex!];
   }
 
-  Onitama.init({this.moveCards}) : super.empty(width: 5, height: 5);
+  Onitama.init({this.moveCards}) : super.empty(width: 5, height: 5) {
+    for (var i = 0; i < 5; i++) {
+      board[0][i] =
+          Pawn(chess: this, chessColor: ChessColor.black, isMaster: i == 2);
+      board[4][i] =
+          Pawn(chess: this, chessColor: ChessColor.white, isMaster: i == 2);
+    }
+
+    if (moveCards != null) {
+      draftCards();
+    }
+  }
+
+  void draftCards() {
+    if (moveCards!.length < 5) {
+      throw Exception("Not enough cards");
+    }
+
+    moveCards!.shuffle();
+
+    for (var draft = 0; draft < 2; draft++) {
+      playerCards[ChessColor.white]!.add(moveCards!.removeLast());
+      playerCards[ChessColor.black]!.add(moveCards!.removeLast());
+    }
+
+    nextCard = moveCards!.removeLast();
+  }
 }
